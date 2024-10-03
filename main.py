b2205c438ebd24df31247d9bd9decb08fb4e4312
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-10-03 20:29:43 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+a = float(input("Me diga o seu número favorito: "))
+if (a < 0): 
+    print("O seu número é negativo.");
+else :
+        print("O seu número é positivo.");
